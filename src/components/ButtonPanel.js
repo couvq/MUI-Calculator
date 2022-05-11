9c51c328caf1e5747a5b472fc09888f43d967fb6
couvq <quentincouvelaire@gmail.com>
2022-05-11 14:13:06 -0400

    button panel row 1 complete

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,3 +1,4 @@
+import { Button } from '@mui/material';
 import React from 'react';
 
 const ButtonPanel = () => {
@@ -11,7 +12,20 @@ const ButtonPanel = () => {
             borderRadius: "15px",
             marginBottom: "2rem"
         }}>
-            ButtonPanel
+            <div style={{
+                display: "flex",
+                gap: "1rem",
+                width: "100%"
+            }}>
+                <Button 
+                sx={{
+                    width: "50%"
+                }}
+                variant='contained'
+                >Clear</Button>
+                <Button variant='contained'>*</Button>
+                <Button variant='contained'>/</Button>
+            </div>
         </div>
     );
 }
