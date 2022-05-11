f09dfe6704b80c1fa6304e648852018d078c2e7c
couvq <quentincouvelaire@gmail.com>
2022-05-11 14:50:45 -0400

    done with button panel

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -8,15 +8,19 @@ const ButtonPanel = () => {
             width: "280px",
             height: "375px",
             zIndex: "1",
-            backgroundColor: "red",
+            // backgroundColor: "red",
             borderRadius: "15px",
-            marginBottom: "2rem"
+            marginBottom: "2rem",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center"
         }}>
             <Stack direction={'row'} spacing={1}>
                 <Stack direction={'column'} spacing={1}>
                     <Stack direction={'row'} spacing={1}>
                         <Button
                         variant='contained'
+                        size='large'
                         sx={{
                             width: "67%"
                         }}
@@ -25,51 +29,57 @@ const ButtonPanel = () => {
                         </Button>
                         <Button
                         variant='contained'
+                        size='large'
                         >
                             *
                         </Button>
                     </Stack>
                     <Stack direction={'row'} spacing={1}>
-                        <Button>7</Button>
-                        <Button>8</Button>
-                        <Button>9</Button>
+                        <Button size='large'>7</Button>
+                        <Button size='large'>8</Button>
+                        <Button size='large'>9</Button>
                     </Stack>
                     <Stack direction={'row'} spacing={1}>
-                        <Button>4</Button>
-                        <Button>5</Button>
-                        <Button>6</Button>
+                        <Button size='large'>4</Button>
+                        <Button size='large'>5</Button>
+                        <Button size='large'>6</Button>
                     </Stack>
                     <Stack direction={'row'} spacing={1}>
-                        <Button>1</Button>
-                        <Button>2</Button>
-                        <Button>3</Button>
+                        <Button size='large'>1</Button>
+                        <Button size='large'>2</Button>
+                        <Button size='large'>3</Button>
                     </Stack>
                     <Stack direction={'row'} spacing={1}>
                         <Button
                         sx={{
                             width: "100%"
                         }}
+                        size='large'
                         >0</Button>
                     </Stack>
                 </Stack>
                 <Stack direction={'column'} spacing={1}>
                     <Button
                     variant='contained'
+                    size='large'
                     >
                         /
                     </Button>
                     <Button
                     variant='contained'
+                    size='large'
                     >
                         -
                     </Button>
                     <Button
                     variant='contained'
+                    size='large'
                     >
                         +
                     </Button>
                     <Button
                     variant='contained'
+                    size='large'
                     sx={{
                         height: "40%"
                     }}
