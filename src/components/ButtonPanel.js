70729a41cd023f448b89d971ae37b0fafc8a2527
couvq <quentincouvelaire@gmail.com>
2022-05-11 14:42:05 -0400

    finally got button panel's content done

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,3 +1,4 @@
+import { Button, Stack } from '@mui/material';
 import React from 'react';
 
 const ButtonPanel = () => {
@@ -11,7 +12,72 @@ const ButtonPanel = () => {
             borderRadius: "15px",
             marginBottom: "2rem"
         }}>
-            
+            <Stack direction={'row'} spacing={1}>
+                <Stack direction={'column'} spacing={1}>
+                    <Stack direction={'row'} spacing={1}>
+                        <Button
+                        variant='contained'
+                        sx={{
+                            width: "67%"
+                        }}
+                        >
+                            Clear
+                        </Button>
+                        <Button
+                        variant='contained'
+                        >
+                            *
+                        </Button>
+                    </Stack>
+                    <Stack direction={'row'} spacing={1}>
+                        <Button>7</Button>
+                        <Button>8</Button>
+                        <Button>9</Button>
+                    </Stack>
+                    <Stack direction={'row'} spacing={1}>
+                        <Button>4</Button>
+                        <Button>5</Button>
+                        <Button>6</Button>
+                    </Stack>
+                    <Stack direction={'row'} spacing={1}>
+                        <Button>1</Button>
+                        <Button>2</Button>
+                        <Button>3</Button>
+                    </Stack>
+                    <Stack direction={'row'} spacing={1}>
+                        <Button
+                        sx={{
+                            width: "100%"
+                        }}
+                        >0</Button>
+                    </Stack>
+                </Stack>
+                <Stack direction={'column'} spacing={1}>
+                    <Button
+                    variant='contained'
+                    >
+                        /
+                    </Button>
+                    <Button
+                    variant='contained'
+                    >
+                        -
+                    </Button>
+                    <Button
+                    variant='contained'
+                    >
+                        +
+                    </Button>
+                    <Button
+                    variant='contained'
+                    sx={{
+                        height: "40%"
+                    }}
+                    >
+                        =
+                    </Button>
+                </Stack>
+            </Stack>
         </div>
     );
 }
